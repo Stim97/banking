8cade7f598a7e2eafb4bcd787359f6e54f5bc178
TimBr <stimbr1777@gmail.com>
2024-08-29 12:44:03 +0500

    Done, and  stopped at 1.18.49 left menu ready

---
+++ components/RightSiderbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSiderbar = ({user, transactions, banks}: RightSidebarProps) => {
+  return (
+    <aside className='right-sidebar'>
+        RightSiderbar
+    </aside>
+  )
+}
+
+export default RightSiderbar
