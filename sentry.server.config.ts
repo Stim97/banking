a5e2e408b33451b5df40d75ca41bea34b05c48a5
TimBr <stimbr1777@gmail.com>
2024-09-04 12:09:14 +0500

    Sentry done, and added some Sentry file and folders as well, after that we ganna start Plaid, stopped at 3.36.00

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://934c802ce5abc8ebed13199a296228cb@o4507893190033408.ingest.de.sentry.io/4507893220048976",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
